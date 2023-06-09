9a01f8ea4e8b52e91ee333775833c9d300c7bb90
Lucas <lidanburg@icloud.com>
2023-06-09 17:30:57 -0400

    created dataset object

--- train.py
+++ train.py
@@ -1,66 +1,64 @@
 import cv2
-import imageio
+from glob import glob
 import numpy as np
 import random
-
-VIDDIR = 'UCF101/'
-VIDPATH = 'batch/train/clip48_2.mp4'
-
-src = cv2.VideoCapture(VIDPATH)
-result = [frame for ret, frame in iter(src.read, (False, None))]
-src.release()
-
-result = np.array(result)[..., [2, 1, 0]]
+import tensorflow as tf
 
 class VideoGen:
-    def __init__(self, dirpath, frames, fps):
+    def __init__(self, dirpath, length, fps):
         """
         dirpath: 'batch/test/' or 'batch/train/'
         frames: CURRENTLY NOT IN USE (will be possibly needed after first training set)
         fps: desired fps of videos post processing (irrelevant after first training set)
         """
         self.dirpath = dirpath
-        self.frames = frames
+        self.length = length
         self.fps = fps
     
     def yield_vids(self):
         """
         Yields full video path, number of white lights
         """
-        for path in random.shuffle(list(self.dirpath.glob('*.mp4'))):
-            yield path, int(path.split('_')[1])
+        vidlist = glob(self.dirpath + '*.mp4')
+        random.shuffle(vidlist)
+        for path in vidlist:
+            yield path, int(path.split('_')[1].split('.')[0])
     
     def get_frames(self, path):
-        video = cv2.VideoCapture(self.dirpath + path)
+        video = cv2.VideoCapture(path)
         frames = []
         index = 0
         # how much to add to index each loop
         # AFTER FIRST ROUND OF TRAINING, STEPPER SHOULD BE A CONSTANT AMOUNT
         # FOR REST OF BATCHES
         stepper = int(video.get(cv2.CAP_PROP_FPS) / self.fps)
-        print(stepper)
-        while index < video.get(cv2.CAP_PROP_FRAME_COUNT):
+        while len(frames) < self.length * self.fps:
             video.set(cv2.CAP_PROP_POS_FRAMES, index)
             ret, frame = video.read()
             frames.append(frame)
             index += stepper
+        video.release()
 
-        src.release()
-
-        return np.array(frames)[..., [2, 1, 0]]
-    
+        return np.array(frames)
     
     def __call__(self):
         """
         Yields a numpy array of frames, number of white lights
         """
-        path, number = self.yield_vids(self)
-        yield self.get_frames(path, self.frames, self.fps), number 
+        for path, number in list(self.yield_vids()):
+            yield self.get_frames(path), number
 
-testVideoGenerator = VideoGen('batch/train/', 200, 1)
+output_sig = (
+    tf.TensorSpec(shape = (None, None, None, 3), dtype = tf.int16),
+    tf.TensorSpec(shape = (), dtype = tf.int8)
+)
 
-for index, frame in enumerate(testVideoGenerator.get_frames('clip48_2.mp4')):
-    imageio.imsave(str(index) + '.png', frame)
+train_ds = tf.data.Dataset.from_generator(
+    VideoGen('batch/train/', 28, 1),
+    output_signature = output_sig
+)
 
-#imageio.imsave('./first.png', result[0])
-#imageio.imsave('./last.png', result[-1])
+frames, labels = next(iter(train_ds))
+print(frames.shape)
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size = tf.data.AUTOTUNE)
