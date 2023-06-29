7e304c876241ca637aa04c9cf8ebd7458821d246
Leah Pascarelli <leahmp2@me.com>
2023-06-29 17:18:44 -0400

    add cropping and 12 secs to chopshop

--- chopshop.py
+++ chopshop.py
@@ -1,19 +1,23 @@
 from moviepy.editor import *
+import os
+import sys
+sys.path.append(os.path.abspath(''))
+from lib.CONSTANTS import SECS
 import math
 
 # constants (EVERYTHING BESIDES COLORS MUST BE EDITED BEFORE EACH BATCH)
 WHITE = [255, 255, 255]
 RED = [228, 20, 0]
 
-LOC1 = (576, 576)
-LOC2 = (576, 617)
-LOC3 = (576, 678)
-BATCHNUM = 2
+LOC1 = (648, 512)
+LOC2 = (648, 576)
+LOC3 = (648, 627)
+BATCHNUM = 1
 LIFTTYPE = "squat"
-STARTTIME = 0
+STARTTIME = 15
 
 # initial objects
-mainClip = VideoFileClip("squat2.mp4").subclip(STARTTIME)
+mainClip = VideoFileClip("longliftingvideo.mp4").subclip(STARTTIME)
 clipseq = 0
 index = 1
 
@@ -38,7 +42,8 @@ while index < mainClip.duration :
                 whiteNum += 1
         
         #chopping up clips
-        newclip = mainClip.subclip(index - 30, index - 2)
+        newclip = mainClip.subclip(index - SECS - 2, index - 2)
+        newclip = newclip.crop(216, 0, 856, 640)
         newclip.write_videofile(
             LIFTTYPE + "-batch" + str(BATCHNUM) + "-" + str(clipseq) + "_" + str(whiteNum) + ".mp4",
             fps = 24,
