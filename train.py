514c4c246c0de01662e91ced2526e22a0345ba20
Lucas Danburg <lidanburg@icloud.com>
2023-07-04 19:30:17 +0000

    filtering out 10% memory warning

--- train.py
+++ train.py
@@ -5,10 +5,13 @@ Args:
     1: str. The path to the saved model folder. 'lifts/squat/models/conv21d/'
     2: str. The path to the lift folder. 'lifts/squat/'
 """
+import os
 from sys import argv
 from tensorflow import keras
 
 from lib.Server import Server
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 server = Server(
     argv[1],
